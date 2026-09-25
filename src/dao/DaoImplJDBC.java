d843e7424db395da5dfdc4746375a3ec621374d6
Eric <huang2423590111@gmail.com>
2026-09-25 17:04:41 +0200

    Definir el metodo getInventory

--- src/dao/DaoImplJDBC.java
+++ src/dao/DaoImplJDBC.java
@@ -6,8 +6,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
+import main.Shop;
 import model.Employee;
+import model.Product;
 
 public class DaoImplJDBC implements Dao {
 	Connection connection;
@@ -60,6 +63,13 @@ public class DaoImplJDBC implements Dao {
 			e.printStackTrace();
 		}
     	return employee;
+	}
+
+	@Override
+	public ArrayList<Product> getInventory() {
+		Shop shop = new Shop();
+		ArrayList<Product> inventory = shop.getInventory();
+		return inventory;
 	}
 
 }
